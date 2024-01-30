4367ac628d6337ee3e8ef4dee52c2e7eda18cafb
brandonhach <specialcambo@gmail.com>
2024-01-30 11:12:34 -0500

    luxon lib

---
+++ models/story.js
@@ -0,0 +1,21 @@
+const { DateTime } = require('luxon');
+
+const stories = [
+	{
+		id: '1',
+		title: 'A funny story',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a viverra orci.',
+		author: 'Brandon',
+		createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+	},
+
+	{
+		id: '2',
+		title: 'It is raining',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a viverra orci.',
+		author: 'Michael',
+		createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+	},
+];
+
+console.log(stories[0]);
